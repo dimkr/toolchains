690090beaaf6b28eb4eca17dc83e28d0266c0358
Dima Krasner <dima@dimakrasner.com>
2019-12-16 09:34:42 +0200

    .travis.yml: deploy all files in out/

--- build-toolchain.sh
+++ build-toolchain.sh
@@ -2,4 +2,5 @@
 
 cp -f config-$TRIPLET .config
 ./crosstool-ng/out/bin/ct-ng build
-tar -c /opt/x-tools/$TRIPLET | xz -9 > $TRIPLET.tar.xz
+mkdir -p out
+tar -c /opt/x-tools/$TRIPLET | xz -9 > out/$TRIPLET.tar.xz
