c8e020c63005a2508d20622c0ed5642c184923a4
Dima Krasner <dima@dimakrasner.com>
2019-12-16 11:10:37 +0200

    build-toolchain.sh: make sure toolchains produce portable binaries

--- build-toolchain.sh
+++ build-toolchain.sh
@@ -2,4 +2,12 @@
 
 cp -f config-$TRIPLET .config
 ./crosstool-ng/out/bin/ct-ng build
+
+grep -e ^CT_TARGET_CFLAGS= -e ^CT_TARGET_LDFLAGS= .config > /tmp/flags
+. /tmp/flags
+/bin/echo -e "#include <stdlib.h>\nint main() {free(malloc(0)); return 1;}" | /opt/x-tools/$TRIPLET/bin/$TRIPLET-gcc $CT_TARGET_CFLAGS $CT_TARGET_LDFLAGS -x c -o /tmp/test -
+file /tmp/test > /tmp/test-$TRIPLET
+readelf -A /tmp/test >> /tmp/test-$TRIPLET
+diff -u test-$TRIPLET /tmp/test-$TRIPLET
+
 tar -c /opt/x-tools/$TRIPLET | xz -9 > $TRIPLET.tar.xz
