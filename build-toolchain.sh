a21a39893cf08f357f9a6bf8e0cdf862f1139a85
Dima Krasner <dima@dimakrasner.com>
2019-12-16 11:36:59 +0200

    build-toolchain.sh: use cross readelf

--- build-toolchain.sh
+++ build-toolchain.sh
@@ -7,7 +7,7 @@ grep -e ^CT_TARGET_CFLAGS= -e ^CT_TARGET_LDFLAGS= .config > /tmp/flags
 . /tmp/flags
 /bin/echo -e "#include <stdlib.h>\nint main() {free(malloc(0)); return 1;}" | /opt/x-tools/$TRIPLET/bin/$TRIPLET-gcc $CT_TARGET_CFLAGS $CT_TARGET_LDFLAGS -x c -o /tmp/test -
 file /tmp/test > /tmp/test-$TRIPLET
-readelf -A /tmp/test >> /tmp/test-$TRIPLET
+/opt/x-tools/$TRIPLET/bin/$TRIPLET-readelf -A /tmp/test >> /tmp/test-$TRIPLET
 diff -u test-$TRIPLET /tmp/test-$TRIPLET
 
 tar -c /opt/x-tools/$TRIPLET | xz -9 > $TRIPLET.tar.xz
