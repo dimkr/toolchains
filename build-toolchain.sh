f5eb02601586b75b12352abaeff2cacab090d219
Dima Krasner <dima@dimakrasner.com>
2019-12-17 14:48:31 +0200

    build-toolchain.sh: make the test executable more useful

--- build-toolchain.sh
+++ build-toolchain.sh
@@ -5,7 +5,7 @@ ct-ng build
 
 grep -e ^CT_TARGET_CFLAGS= -e ^CT_TARGET_LDFLAGS= .config > /tmp/flags
 . /tmp/flags
-/bin/echo -e "#include <stdlib.h>\nint main() {free(malloc(0)); return 1;}" | /opt/x-tools/$TRIPLET/bin/$TRIPLET-gcc $CT_TARGET_CFLAGS $CT_TARGET_LDFLAGS -x c -o /tmp/test -
+/bin/echo -e "#include <stdio.h>\n#include <stdlib.h>\n#include <math.h>\n#include <time.h>\nint main() {puts(\"hello\"); free(malloc(1)); return (int)floor((double)time(NULL)/3);}" | /opt/x-tools/$TRIPLET/bin/$TRIPLET-gcc $CT_TARGET_CFLAGS -x c -o /tmp/test - $CT_TARGET_LDFLAGS -lm
 file /tmp/test > /tmp/test-$TRIPLET
 /opt/x-tools/$TRIPLET/bin/$TRIPLET-readelf -A /tmp/test >> /tmp/test-$TRIPLET
 diff -u test-$TRIPLET /tmp/test-$TRIPLET
