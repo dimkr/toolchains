f9712e36b7ab32bec1c4c46350c278bc402b9ad0
Dima Krasner <dima@dimakrasner.com>
2020-01-14 20:56:04 +0200

    build-toolchain.sh: add a venv-style activate script

--- build-toolchain.sh
+++ build-toolchain.sh
@@ -15,7 +15,17 @@ ct-ng build
 
 grep -e ^CT_TARGET_CFLAGS= -e ^CT_TARGET_LDFLAGS= .config > /tmp/flags
 . /tmp/flags
-/bin/echo -e "#include <stdio.h>\n#include <stdlib.h>\n#include <math.h>\n#include <time.h>\nint main() {puts(\"hello\"); free(malloc(1)); return (int)floor((double)time(NULL)/3);}" | /opt/x-tools/$TRIPLET/bin/$TRIPLET-gcc $CT_TARGET_CFLAGS -x c -o hello-$TRIPLET - $CT_TARGET_LDFLAGS -lm
+cat << EOF > /opt/x-tools/$TRIPLET/activate
+#!/bin/sh
+
+export PATH=\$PATH:/opt/x-tools/$TRIPLET/bin
+export CFLAGS="$CT_TARGET_CFLAGS \$CFLAGS"
+export LDFLAGS="$CT_TARGET_LDFLAGS \$LDFLAGS"
+EOF
+chmod 755 /opt/x-tools/$TRIPLET/activate
+
+. /opt/x-tools/$TRIPLET/activate
+/bin/echo -e "#include <stdio.h>\n#include <stdlib.h>\n#include <math.h>\n#include <time.h>\nint main() {puts(\"hello\"); free(malloc(1)); return (int)floor((double)time(NULL)/3);}" | $TRIPLET-gcc $CFLAGS -x c -o hello-$TRIPLET - $LDFLAGS -lm
 file hello-$TRIPLET > /tmp/test-$TRIPLET
 /opt/x-tools/$TRIPLET/bin/$TRIPLET-readelf -A hello-$TRIPLET >> /tmp/test-$TRIPLET
 diff -u test-$TRIPLET /tmp/test-$TRIPLET
